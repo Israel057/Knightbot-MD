532f9f8ac6af5a74caecf520f925fc3371a9e834
Israel057 <owensetinosa4@gmail.com>
2025-02-06 09:04:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Lite Tv', // Your name
-  ownerNumber: '+2349076792300', //Your number
+  ownerNumber: '2349076792300', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
